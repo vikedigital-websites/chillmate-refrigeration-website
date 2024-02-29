47c7f271ed629860f409b23be81127112b319dfa
Luke6905 <l_holmgren@live.com.au>
2024-02-29 20:58:16 +1100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useMediaQueries, useMediaQuery } from "@react-hook/media-query";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { IoMenu as MenuIcon } from "react-icons/io5";
 
 const navItems = [
@@ -15,16 +15,21 @@ const navItems = [
 
 export default function Home() {
   const isSmallScreen = useMediaQuery('(max-width: 1120px)')
+  const [isClient, setIsClient] = useState (false)
+ 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   return (
     <>
       <a href="tel: 0422407129" className="bg-primary block text-white p-2 text-center w-screen">CALL DYLAN ON: <b>04 2240 7129</b></a>
-      <main className="w-[1120px] px-4">
+      <main className="w-full max-w-[1120px] px-4">
 
-        <header className="h-28 flex space-b justify-between items-center py-4">
-          <Image src={'/logo.svg'} alt="Chillmate Logo" width={0} height={0} id="logo" className="w-auto h-full"></Image>
-          {!isSmallScreen && <nav className="flex gap-4">{navItems}</nav>}
-          {isSmallScreen && <MenuIcon />}
+        <header className="h-20 md:h28 flex space-b justify-between items-center py-4">
+          <Image src={'/logo.svg'} alt="Chillmate Logo" width={0} height={0} id="logo" className="w-auto h-full" priority></Image>
+          {isClient && !isSmallScreen && <nav className="flex gap-4">{navItems}</nav>}
+          {isClient && isSmallScreen && <MenuIcon />}
         </header>
       </main>
     </>
