86b647472b0d6c9c91fd56f226a59bb522362e09
lukeholmgren <l_holmgren@live.com.au>
2024-03-09 10:26:32 +1100

    updated for gh Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: "export",
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
 
-  // basePath: "/chillmate-refrigeration-website",
-  // assetPrefix: "/chillmate-refrigeration-website/",
+    basePath: "/chillmate-refrigeration-website",
+    assetPrefix: "/chillmate-refrigeration-website/",
 
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
