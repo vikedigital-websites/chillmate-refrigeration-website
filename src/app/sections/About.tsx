f44c3c99c9b2ab2ee06b001fb0f00017ef8b354c
lukeholmgren <l_holmgren@live.com.au>
2024-03-13 12:33:10 +1100

    changed about section to new layout

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -11,10 +11,11 @@ const About = (props: Props) => {
     return (
         <section id='about-section' className='relative flex w-full flex-col items-center gap-16'>
             <AnchorMagnet sectionId='about' />
-            <SectionHeading>About Us</SectionHeading>
+
             <div className='flex flex-col gap-16 lg:flex-row'>
                 <Image src={palceholderImg} alt='' width={0} height={0} id='logo' className='h-auto w-full rounded-xl' priority></Image>
-                <div className='flex flex-col justify-between gap-16'>
+                <div className='flex flex-col justify-between gap-8'>
+                    <SectionHeading>About Us</SectionHeading>
                     <p className='text-center leading-loose md:text-start'>
                         At ChillMate, we take pride in our specialization in comprehensive services, which encompass maintenance, installations, and repairs. With over 8 years of industry expertise under our belt, you can trust us to keep
                         your systems in optimal condition, ensuring reliable performance year-round. Moreover, we prioritize safety and compliance by holding all the necessary licenses, providing our customers with peace of mind and
